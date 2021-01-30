9571120dd9a85a346a224633efcd85e94b36174f
admin <moeddie@hotmail.com>
2021-01-30 00:45:58 -0800

    copy pasta

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using GradeBook.Enums;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
@@ -10,18 +14,18 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if (Students.Count < 5)
-            {
-                throw new InvalidOperationException("Students are less than 5");
-            }
-            var grade = 'F';
+            if (Students.Count < 5) throw new InvalidOperationException("Students are less than 5");
 
-            if (averageGrade >= 80) return grade = 'A';
-            if (averageGrade >= 60 && averageGrade < 80) return grade = 'B';
-            if (averageGrade >= 40 && averageGrade < 60) return grade = 'C';
-            if (averageGrade >= 0 && averageGrade < 40) return grade = 'D';
+            // good ol copy pasting here
+            var limit = (int)Math.Ceiling(Students.Count * 0.2);
 
-            return grade;
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[limit - 1] <= averageGrade) return 'A';
+            else if (grades[(limit * 2) - 1] <= averageGrade) return 'B';
+            else if (grades[(limit * 3) - 1] <= averageGrade) return 'C';
+            else if (grades[(limit * 4) - 1] <= averageGrade) return 'D';
+            else return 'F';
         }
     }
 }

df1c3a4c61b840e7142dd3fd1d16f02eb3b3c418
admin <moeddie@hotmail.com>
2021-01-30 00:25:33 -0800

    grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,5 +7,21 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Students are less than 5");
+            }
+            var grade = 'F';
+
+            if (averageGrade >= 80) return grade = 'A';
+            if (averageGrade >= 60 && averageGrade < 80) return grade = 'B';
+            if (averageGrade >= 40 && averageGrade < 60) return grade = 'C';
+            if (averageGrade >= 0 && averageGrade < 40) return grade = 'D';
+
+            return grade;
+        }
     }
 }
